209faf54eda3e6a7887d820d5de944322cd840b8
Eric Samuelsson <gossen02@gmail.com>
2022-09-23 13:02:12 +0100

    Split long line

--- db/database.js
+++ db/database.js
@@ -3,7 +3,8 @@ const collectionName = "documents";
 
 const database = {
     getDb: async function getDb() {
-        let dsn = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.r93kcrs.mongodb.net/?retryWrites=true&w=majority`;
+        let dsn = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}
+        @cluster0.r93kcrs.mongodb.net/?retryWrites=true&w=majority`;
 
         if (process.env.NODE_ENV === 'test') {
             dsn = "mongodb://localhost:27017/test";
